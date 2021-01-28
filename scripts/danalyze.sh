c0ac0ed74098448d837fc83666d5f5f57b07d8ae
Dmitrii Leliuhin <dleliuhin@gmail.com>
2021-01-28 16:39:12 +0300

    Fix. yaml

--- scripts/danalyze.sh
+++ scripts/danalyze.sh
@@ -5,12 +5,12 @@ scripts/tests.sh
 cd tests/build_test
 
 echo -e '<<<<<<<<<<<<<<<<<<<<< MEMCHECK ANALYZE >>>>>>>>>>>>>>>>>>>>>>>>>>>\n'
-timeout 5 valgrind --tool=memcheck --leak-check=full --leak-resolution=low --undef-value-errors=yes ./test_vsimple
+timeout 20 valgrind --tool=memcheck --leak-check=full --leak-resolution=low --undef-value-errors=yes ./test_vsimple
 
 echo
 echo -e '<<<<<<<<<<<<<<<<<<<<< CACHEGRIND ANALYZE >>>>>>>>>>>>>>>>>>>>>>>>>\n'
-timeout 5 valgrind --tool=cachegrind ./test_vsimple
+timeout 20 valgrind --tool=cachegrind ./test_vsimple
 
 echo
 echo -e '<<<<<<<<<<<<<<<<<<<<< MASSIF ANALYZE >>>>>>>>>>>>>>>>>>>>>>>>>\n'
-timeout 5 valgrind --tool=massif ./test_vsimple
+timeout 20 valgrind --tool=massif ./test_vsimple
